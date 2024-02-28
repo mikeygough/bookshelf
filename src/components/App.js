c926db9293138bd402f7afc653cbb186dda3104f
mikey <mikey@hey.com>
2024-02-28 07:21:54 -0600

    add search. more styling

--- src/components/App.js
+++ src/components/App.js
@@ -4,7 +4,6 @@ import { Outlet } from 'react-router-dom';
 import './App.css';
 import Header from './Header/Header';
 import Footer from './Footer/Footer';
-// import Bookshelf from './Bookshelf/Bookshelf';
 
 function App() {
   return (
--- src/components/Bookshelf/Bookshelf.js
+++ src/components/Bookshelf/Bookshelf.js
@@ -1,25 +1,50 @@
 import React from 'react';
+import { useState } from 'react';
 import Book from '../Book/Book';
 import './Bookshelf.css';
 
 import data from '../../book-data.js';
 
 function Bookshelf() {
-  // deconstruct inside the map
-  const books = data.map(({ id, title, author, image, review }) => {
-    return (
-      <Book
-        id={id}
-        key={id}
-        title={title}
-        author={author}
-        image={image}
-        review={review}
-      />
-    );
-  });
+  const [query, setQuery] = useState('');
 
-  return <div className="Bookshelf">{books}</div>;
+  const books = data
+    .filter(
+      (obj) =>
+        obj.title.toLowerCase().includes(query.toLowerCase()) ||
+        obj.author.toLowerCase().includes(query.toLowerCase())
+    )
+    .sort((a, b) => a.title.localeCompare(b.title))
+    .map(({ id, title, author, image, review }) => {
+      return (
+        <Book
+          id={id}
+          key={id}
+          title={title}
+          author={author}
+          image={image}
+          review={review}
+        />
+      );
+    });
+
+  return (
+    <div className="Bookshelf-Wrapper">
+      <form>
+        <input
+          value={query}
+          placeholder="Search for a Book!"
+          onChange={(e) => setQuery(e.target.value)}
+        />
+        <button type="submit">Search</button>
+      </form>
+      <div className="Bookshelf">
+        {books.length > 0
+          ? books
+          : 'No Books Match Your Search Query...'}
+      </div>
+    </div>
+  );
 }
 
 export default Bookshelf;
